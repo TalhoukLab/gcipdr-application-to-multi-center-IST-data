37117ec14229a58ee2f870438c1daba80594c95d
Derek Chiu <dchiu@bccrc.ca>
2020-11-03 12:35:29 -0800

    Update sim_2.R

--- sim_2.R
+++ sim_2.R
@@ -1,4 +1,4 @@
-## sim_2.R contains R commands to execute 'gcipdr application to multi-center IST data' (from homonymous repository). 
+## sim_2.R contains R commands to execute 'gcipdr application to multi-center IST data' (from homonymous repository).
 ## Copyright (C) 2018 Federico Bonofiglio
 
     ## This Program is free software: you can redistribute it and/or modify
@@ -21,7 +21,7 @@
 
 
 
-# R version 3.6.1 (2019-07-05) -- "Action of the Toes"  
+# R version 3.6.1 (2019-07-05) -- "Action of the Toes"
 
 
 
@@ -39,16 +39,19 @@ source("simulation_functions_v9.R")
 
 
 ## simulations parameters
+if (.Platform$OS.type == "windows") {
+    ncores <- 1
+} else {
+    ncores <- 28 # BEWARE: changing Nr cores can affect random seeds and give different results
+}
 
-ncores <- 28 # BEWARE: changing Nr cores can affect random seeds and give different results
-
- params <- data.frame(n = rep(c(100, 500, 1000), rep(2,3)), K = rep(c(2, 10), 3) )
+params <- data.frame(n = rep(c(100, 500, 1000), rep(2,3)), K = rep(c(2, 10), 3) )
 
 R <- 100
 
 ## rxasp
 
- system.time( ppp <- TabulateSimulations(R, params, ncores = ncores, latextable = FALSE, beta0_I = -1.1, beta0_II = c(-2, 0), betarsbp_I = -0.3, betarsbp_II = c(-0.1, -0.4), binary_covariate = TRUE ) )  
+system.time( ppp <- TabulateSimulations(R, params, ncores = ncores, latextable = FALSE, beta0_I = -1.1, beta0_II = c(-2, 0), betarsbp_I = -0.3, betarsbp_II = c(-0.1, -0.4), binary_covariate = TRUE ) )
 
 # in alternative analsis was also set betarsbp_II = c(-0.04, -0.4)
 
@@ -65,10 +68,10 @@ rxasp_simul[which(ppp$central$rxasp["params"] != "$\\sigma_{b}$"), ] <- apply(pp
 rxasp_simul[which(ppp$central$rxasp["params"] == "$\\sigma_{b}$"), ] <- apply(ppp$central$`(Intercept)`[ppp$central$`(Intercept)`["params"] == "$\\sigma_{b}$", ], 2, as.character)
 
  rxaspsimul <- data.frame(rxasp_simul); colnames(rxaspsimul) <- colnames(ppp$central$rxasp)
- 
+
 rxaspsimulsm <- rxaspsimul[-c(7:12, 25:30), ]
 
- levels(rxaspsimulsm$copula.data)[2:3] <- c("\\parbox[t]{2mm}{\\multirow{12}{*}{\\rotatebox[origin=c]{90}{Mismodeled}}}",  
+ levels(rxaspsimulsm$copula.data)[2:3] <- c("\\parbox[t]{2mm}{\\multirow{12}{*}{\\rotatebox[origin=c]{90}{Mismodeled}}}",
  "\\parbox[t]{2mm}{\\multirow{12}{*}{\\rotatebox[origin=c]{90}{Well modeled}}}")
 
 ## small error with multirow em
