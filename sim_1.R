6d74f06a135941d4b735e7a5042a3f43bdfd8ad1
Derek Chiu <dchiu@bccrc.ca>
2020-11-03 12:34:57 -0800

    Update sim_1.R

--- sim_1.R
+++ sim_1.R
@@ -1,4 +1,4 @@
-## sim_1.R contains R commands to execute 'gcipdr application to multi-center IST data' (from homonymous repository). 
+## sim_1.R contains R commands to execute 'gcipdr application to multi-center IST data' (from homonymous repository).
 ## Copyright (C) 2018 Federico Bonofiglio
 
     ## This Program is free software: you can redistribute it and/or modify
@@ -19,7 +19,7 @@
 ############ MULTI-CENTER STRUCTURE ....
 # Blood pressure effect
 
-# R version 3.6.1 (2019-07-05) -- "Action of the Toes"  
+# R version 3.6.1 (2019-07-05) -- "Action of the Toes"
 
 library(gcipdr)
 
@@ -35,19 +35,22 @@ source("simulation_functions_v9.R")
 
 
 ## simulations parameters
+if (.Platform$OS.type == "windows") {
+    ncores <- 1
+} else {
+    ncores <- 28 # BEWARE: changing Nr cores can affect random seeds and give different results
+}
 
-ncores <- 28 # BEWARE: changing Nr cores can affect random seeds and give different results
-
- params <- data.frame(n = rep(c(100, 500, 1000), rep(2,3)), K = rep(c(2, 10), 3) )
+params <- data.frame(n = rep(c(100, 500, 1000), rep(2,3)), K = rep(c(2, 10), 3) )
 
 R <- 100
 
- system.time( ppp <- TabulateSimulations(R, params, ncores = ncores, latextable = FALSE) ) 
+system.time( ppp <- TabulateSimulations(R, params, ncores = ncores, latextable = FALSE) )
 
 ## saveRDS(ppp, "simul_1_final.rds")
 
 
-### re-arrange table 
+### re-arrange table
 
 rsbp_simul <- matrix( nrow = dim(ppp$central$rsbp)[1], ncol = dim(ppp$central$rsbp)[2] )
 
@@ -56,10 +59,10 @@ rsbp_simul[which(ppp$central$rsbp["params"] != "$\\sigma_{b}$"), ] <- apply(ppp$
 rsbp_simul[which(ppp$central$rsbp["params"] == "$\\sigma_{b}$"), ] <- apply(ppp$central$`(Intercept)`[ppp$central$`(Intercept)`["params"] == "$\\sigma_{b}$", ], 2, as.character)
 
  rsbpsimul <- data.frame(rsbp_simul); colnames(rsbpsimul) <- colnames(ppp$central$rsbp)
- 
+
 rsbpsimulsm <- rsbpsimul[-c(7:12, 25:30), ]
 
- levels(rsbpsimulsm$copula.data)[2:3] <- c("\\parbox[t]{2mm}{\\multirow{12}{*}{\\rotatebox[origin=c]{90}{Mismodeled}}}",  
+ levels(rsbpsimulsm$copula.data)[2:3] <- c("\\parbox[t]{2mm}{\\multirow{12}{*}{\\rotatebox[origin=c]{90}{Mismodeled}}}",
  "\\parbox[t]{2mm}{\\multirow{12}{*}{\\rotatebox[origin=c]{90}{Well modeled}}}")
 
 ## small error with multirow em
